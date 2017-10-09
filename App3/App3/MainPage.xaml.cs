d72445181e8096e27043b71ba1937145ab36e762
Your Name <you@example.com>
2017-10-09 17:32:29 -0500

    First commit and seems to be working

---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+	public partial class MainPage : ContentPage
+    {
+        private Context context;
+        public int index;
+        
+
+        public MainPage()
+        {
+            MainProcess();
+
+        }
+
+        private void MainProcess()
+        {
+            initialise();
+
+        }
+
+        private void initialise()
+        {
+            //InitializeComponent();
+            var layout = new StackLayout();
+            var label = new Label { Text = "Name?" };
+            var namefield = new Entry { Text = "" };
+
+            Picker male = new Picker
+            {
+                Title = "Male",
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            male.Items.Add("3");
+            male.Items.Add("5");
+            male.Items.Add("10");
+
+            this.Content = layout;
+            layout.Children.Add(label);
+            layout.Children.Add(namefield);
+            layout.Children.Add(male);
+
+            male.SelectedIndexChanged += (sender, args) =>
+            {
+                switch (male.SelectedIndex)
+                {
+                    case 0:
+                        index = 3;
+                        break;
+                    case 1:
+                        index = 5;
+                        break;
+                    case 2:
+                        index = 10;
+                        break;
+
+                }
+                /*if (male.SelectedIndex == 0)
+                {
+                    index = 3;
+                }
+                else
+                {
+
+                }
+                index = male.SelectedIndex;*/
+            };
+
+            var button = new Xamarin.Forms.Button
+            {
+                Text = "Hallo, presiona el button, please.",
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            button.Clicked += Button_Clicked;
+            layout.Children.Add(button);
+        }
+
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            Page1 pgn = new Page1(index);
+            await Navigation.PushAsync(pgn);
+        }
+
+
+    }
+}
---
+++ App3/App3/Page1.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+        private int index;
+        private MainPage mnpg;
+        private int score = 0;
+        private int result;
+        private int it = 1;
+        private int answer;
+        Entry antwort = new Entry { };
+
+
+        public Page1 (int index)
+		{
+            this.index = index;
+            Console.WriteLine(index + ""+ it);
+			InitializeComponent ();
+            game();
+		}
+
+        async private void game()
+        {
+            if (index == 0)
+            {
+                await DisplayAlert("Alert", "Tell me how many times or I won't budge >:(", "OK");
+                await Navigation.PopAsync();
+            }
+            else
+            {
+                Random rand1 = new Random();
+
+                var label = new Label { Text = it.ToString() + " de " + index.ToString() };
+
+                String expo;
+
+                int frst = rand1.Next(100);
+                int scnd = rand1.Next(100);
+                int exp = rand1.Next(1);
+
+                var layout = new StackLayout();
+                this.Content = layout;
+                //var label = new Label { Text = "Ich habe das Fernseher wechseln!" };
+
+                if (exp == 0)
+                {
+                    expo = "+";
+                    answer = frst + scnd;
+                }
+                else
+                {
+                    expo = "-";
+                    answer = frst - scnd;
+                }
+
+                var operation = new Label { Text = frst.ToString() + expo + scnd.ToString() };
+
+                var button = new Xamarin.Forms.Button
+                {
+                    Text = "Check",
+                    VerticalOptions = LayoutOptions.Start,
+                    HorizontalOptions = LayoutOptions.FillAndExpand
+                };
+                button.Clicked += Calculate;
+
+                var backbttn = new Xamarin.Forms.Button
+                {
+                    Text = "Back again",
+                    VerticalOptions = LayoutOptions.Start,
+                    HorizontalOptions = LayoutOptions.FillAndExpand
+                };
+                backbttn.Clicked += Bttn;
+
+                layout.Children.Add(label);
+                layout.Children.Add(operation);
+                layout.Children.Add(antwort);
+                layout.Children.Add(button);
+                layout.Children.Add(backbttn);
+
+            }
+        }
+
+        async private void Calculate(object sender, EventArgs e)
+        {
+            if (it < index+1)
+            {
+                if (Convert.ToInt32(antwort.Text) == answer)
+                {
+                    score = score + 1;
+                }
+                antwort.Text = "";
+                it = it + 1;
+                game();
+            }
+            else
+            {
+                await DisplayAlert("Game over", score.ToString() + " correct over " + index.ToString(), "OK");
+                score = 0;
+                it = 1;
+                await Navigation.PopAsync();
+            }
+        }
+
+        async private void Bttn(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
